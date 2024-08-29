7e5dcac417455db85e24dd07180b5c3bd9503f78
louol <xlouolx@gmail.com>
2024-08-29 19:41:36 +0800

    lesson_2_2

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second == third:
+    print('Все числа равны - 3')
+if first == second or first == third or second == third:
+    print('Два числа равны - 2')
+else:
+    print('Равных чисел нет - 0')
