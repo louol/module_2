0ddfde4f6e8ec7ceee8b37c143b43f07d56cd86d
louol <xlouolx@gmail.com>
2024-10-02 03:21:01 +0800

    lesson_5_3_modifi

--- module_5_3.py
+++ module_5_3.py
@@ -8,31 +8,42 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other
+        if isinstance(other, (int, House)):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return  self.number_of_floors < other
+        if isinstance(other, (int, House)):
+            return  self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other
+        if isinstance(other, (int, House)):
+            return self.number_of_floors <= other
 
     def __gt__(self, other):
-        return self.number_of_floors > other
+        if isinstance(other, (int, House)):
+            return self.number_of_floors > other
 
     def __ge__(self, other):
-        return self.number_of_floors >= other
+        if isinstance(other, (int, House)):
+            return self.number_of_floors >= other
 
     def __ne__(self, other):
-        return self.number_of_floors != other
+        if isinstance(other, (int, House)):
+            return self.number_of_floors != other
 
     def __add__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, (int, House)):
+            return House(self.name, self.number_of_floors + value)
 
     def __radd__(self, value):
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, (int, House)):
+            return House(self.name, self.number_of_floors + value)
 
     def __iadd__(self, value):
-        return House(self.name, value + self.number_of_floors)
+        if isinstance(value, (int, House)):
+            return House(self.name, value + self.number_of_floors)
+
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
